7fe35b7b6da261dd3bdd7f7965e3d47c782ad2f4
Reinier Schoof <reinier@skoef.nl>
2014-12-09 13:05:36 +0100

    added some quotes

--- tests.sh
+++ tests.sh
@@ -5,24 +5,24 @@ FIXTURE_PATH="tests/fixtures"
 
 setup() {
     # create path for this test
-    export STAMP_PATH=$(mktemp -d /tmp/stamp.${BATS_TEST_NAME}.XXX)
-    mkdir -p ${STAMP_PATH}
+    export STAMP_PATH=$(mktemp -d "/tmp/stamp.${BATS_TEST_NAME}.XXX")
+    mkdir -p "${STAMP_PATH}"
 
     # possible fixtures for this test
-    export FIXTURE_TXT=${FIXTURE_PATH}/${BATS_TEST_NAME}.txt
-    export FIXTURE_SUM=${FIXTURE_PATH}/${BATS_TEST_NAME}.sum
+    export FIXTURE_TXT="${FIXTURE_PATH}/${BATS_TEST_NAME}.txt"
+    export FIXTURE_SUM="${FIXTURE_PATH}/${BATS_TEST_NAME}.sum"
 }
 
 @test "create note" {
     run ${STAMP} -a foobar testing
     shouldbe=$(date "+1%t%Y-%m-%d%ttesting")
-    run cat ${STAMP_PATH}/foobar
+    run cat "${STAMP_PATH}/foobar"
     [ $shouldbe = $lines ]
 }
 
 @test "create note with custom date" {
     run ${STAMP} -a foobar testing 1970-01-01
-    run cmp ${STAMP_PATH}/foobar ${FIXTURE_TXT}
+    run cmp "${STAMP_PATH}/foobar" ${FIXTURE_TXT}
     [ $status -eq 0 ]
 }
 
@@ -31,7 +31,7 @@ setup() {
     run ${STAMP} -a foobar testing2
     run ${STAMP} -a foobar testing3 2014-12-09
     run ${STAMP} -d foobar 2
-    run cmp ${STAMP_PATH}/foobar ${FIXTURE_TXT}
+    run cmp "${STAMP_PATH}/foobar" ${FIXTURE_TXT}
     [ $status -eq 0 ]
 }
 
@@ -39,9 +39,9 @@ setup() {
     skip "no way to confirm from stdin yet"
     run ${STAMP} -a foobar testing1
     run ${STAMP} -D foobar
-    [ -f ${STAMP_PATH}/foobar ] && [ ! -s ${STAMP_PATH}/foobar ]
+    [ -f "${STAMP_PATH}/foobar" ] && [ ! -s "${STAMP_PATH}/foobar" ]
 }
 
 teardown() {
-    rm -r ${STAMP_PATH}
+    rm -r "${STAMP_PATH}"
 }
